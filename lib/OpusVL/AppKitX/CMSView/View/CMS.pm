bf1a5a36222b32bfc278936ade9ac187c409aed5
Brad <brad@geeksware.net>
2012-11-30 15:44:51 +0000

    Updated CMS::View to TT::Alloy

--- lib/OpusVL/AppKitX/CMSView/View/CMS.pm
+++ lib/OpusVL/AppKitX/CMSView/View/CMS.pm
@@ -2,10 +2,11 @@ package OpusVL::AppKitX::CMSView::View::CMS;
 
 use Moose;
 
-extends 'Catalyst::View::TT';
+extends 'Catalyst::View::TT::Alloy';
 
 __PACKAGE__->config({
   ENCODING => 'utf-8',
+  AUTO_FILTER => 'html',
 });
 
 1;
