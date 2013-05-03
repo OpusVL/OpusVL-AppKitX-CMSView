cd2dd2205d208a5851ca47b7808564ca5e48fb2e
Brad <brad@geeksware.net>
2013-05-03 11:24:21 +0100

    Added mobile redirection stuff

--- lib/OpusVL/AppKitX/CMSView/Controller/CMS/Root.pm
+++ lib/OpusVL/AppKitX/CMSView/Controller/CMS/Root.pm
@@ -8,15 +8,27 @@ BEGIN { extends 'OpusVL::AppKit::Controller::Root'; };
  
 __PACKAGE__->config( namespace => '');
 
+sub _is_mobile_device :Private {
+    my ($self, $c) = @_;
+    my $agent      = $c->req->user_agent;
+    if ($agent =~ m/android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|meego.+mobile|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i || substr($agent, 0, 4) =~ m/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i) {
+        return 1;
+    }
+}
+
+
 sub default :Private {
     my ($self, $c) = @_;
+    my $is_mobile = 0;
+    my $orig_dom;
+    my $domain;
     my $url   = '/' . $c->req->path;
     my $host  = $c->req->uri->host;
     my $site;
 
     if ($url =~ /^\/_asset\//) {
-	my @args = @{$c->req->arguments};
-	shift @args;
+	    my @args = @{$c->req->arguments};
+	    shift @args;
         return $self->_asset($c, @args);
     }
 
@@ -43,6 +55,61 @@ sub default :Private {
         $c->detach;
     }
 
+    my $pages = $site->pages;
+    my $page = $pages->search({ site => $site->id })->published->find({url => $url});
+
+    # using a mobile device
+    if ($c->config->{mobile_redirection_service}) {
+        if ($self->_is_mobile_device($c)) {
+            ## uncomment this for full cmsview mobiel redirection compatibility
+            # check to see if they're on a fullsite (no m. domain)
+            #if ($host !~ /^m\./) {
+            #    # is there a mobile site?
+            #    if (my $dom = $c->model('CMS::MasterDomain')->find({ domain => "m.${host}" })) {
+            #        my $full_site = $dom->site;
+            #        # is there a mobile page for it?
+            #        # if so, then redirect to that
+            #        if (my $chk_page = $full_site->pages->search({ site => $full_site->id })->published->find({ url => $url })) {
+            #            my $prot = $c->req->uri->secure ? 'https://' : 'http://';
+            #            my $port = $c->req->uri->port;
+            #            $c->res->redirect("${prot}m.${host}:${port}${url}");
+            #            $c->detach;
+            #        }
+            #        # no? then is there a full site page for it?
+            #        # and if not, send to mobile 404 page
+            #        if (not $page) {
+            #            $c->res->redirect('/');
+            #            $c->detach;
+            #        }
+            #    } # end mobile site check
+            #} # end fullsite check
+            if ($host =~ /^m\./) {
+                # get the full host (without the m. bit)
+                my $fullhost = substr $host, 2;
+                # if the mobile page does not exist
+                # else, render as normal
+                if (not $page) {
+                    # does a full site exist?
+                    if (my $dom = $c->model('CMS::MasterDomain')->find({ domain => $fullhost })) {
+                        # try to get the page and go there
+                        my $full_site = $dom->site;
+                        if (my $chk_page = $full_site->pages->search({ site => $full_site->id })->published->find({ url => $url })) {
+                           my $prot = $c->req->uri->secure ? 'https://' : 'http://';
+                           my $port = $c->req->uri->port;
+                           $c->res->redirect("${prot}${fullhost}:${port}${url}");
+                           $c->detach;
+                        }
+                        # or show mobile 404
+                        else {
+                            $c->res->redirect('/');
+                            $c->detach;
+                        }
+                    }   
+                }
+            }
+        } # end mobile device check
+    } # end mobile_redirection_service config check
+
     $c->log->debug("********** Running CMS lookup against: ${url} @ ${host}");
 
     # Does the URL match a page alias?
@@ -51,10 +118,6 @@ sub default :Private {
         $c->res->redirect($c->uri_for($alias->page->url), 301);
         $c->detach;
     }
-    
-    my $pages = $site->pages;
-    # Does the URL match a real page?
-    my $page = $pages->search({ site => $site->id })->published->find({url => $url});
     
     # If not, do we have a page matching the current action?
     $page //= do {
