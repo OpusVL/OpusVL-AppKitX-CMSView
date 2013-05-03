36c1fcb2429e90f9ed240f56304efadb1c75e064
Brad <brad@geeksware.net>
2013-05-03 14:51:20 +0100

    Added more mobile redirection stuff

--- lib/OpusVL/AppKitX/CMSView/Controller/CMS/Root.pm
+++ lib/OpusVL/AppKitX/CMSView/Controller/CMS/Root.pm
@@ -61,31 +61,48 @@ sub default :Private {
     # using a mobile device
     if ($c->config->{mobile_redirection_service}) {
         if ($self->_is_mobile_device($c)) {
-            ## uncomment this for full cmsview mobiel redirection compatibility
+            # uncomment this for full cmsview mobil redirection compatibility
             # check to see if they're on a fullsite (no m. domain)
-            #if ($host !~ /^m\./) {
-            #    # is there a mobile site?
-            #    if (my $dom = $c->model('CMS::MasterDomain')->find({ domain => "m.${host}" })) {
-            #        my $full_site = $dom->site;
-            #        # is there a mobile page for it?
-            #        # if so, then redirect to that
-            #        if (my $chk_page = $full_site->pages->search({ site => $full_site->id })->published->find({ url => $url })) {
-            #            my $prot = $c->req->uri->secure ? 'https://' : 'http://';
-            #            my $port = $c->req->uri->port;
-            #            $c->res->redirect("${prot}m.${host}:${port}${url}");
-            #            $c->detach;
-            #        }
-            #        # no? then is there a full site page for it?
-            #        # and if not, send to mobile 404 page
-            #        if (not $page) {
-            #            $c->res->redirect('/');
-            #            $c->detach;
-            #        }
-            #    } # end mobile site check
-            #} # end fullsite check
-            if ($host =~ /^m\./) {
+            if ($host !~ /^m\./) {
+                # is there a mobile site?
+                if (my $dom = $c->model('CMS::MasterDomain')->find({ domain => "m.${host}" })) {
+                    my $full_site = $dom->site;
+                    # is there a mobile page for it?
+                    # if so, then redirect to that
+                    if (my $chk_page = $full_site->pages->search({ site => $full_site->id })->published->find({ url => $url })) {
+                        my $prot = $c->req->uri->secure ? 'https://' : 'http://';
+                        my $port = $c->req->uri->port;
+                        $c->res->redirect("${prot}m.${host}:${port}${url}");
+                        $c->detach;
+                    }
+                    # no? then is there a full site page for it?
+                    # and if not, send to mobile 404 page
+                    if (not $page) {
+                        $c->res->redirect('/');
+                        $c->detach;
+                    }
+                } # end mobile site check
+            } # end fullsite check
+            elsif ($host =~ /^m\./) {
                 # get the full host (without the m. bit)
                 my $fullhost = substr $host, 2;
+                
+                # requested full site?
+                if ($c->req->query_params->{mwf} eq 't') {
+                    $c->session->{mobile_fullsite_pls} = 1;
+                    # does a full site exist?                                                                                                                                               
+                    if (my $dom = $c->model('CMS::MasterDomain')->find({ domain => $fullhost })) {
+                        # try to get the page and go there                                                                                                                                  
+                        my $full_site = $dom->site;
+                        if (my $chk_page = $full_site->pages->search({ site => $full_site->id })->published->find({ url => $url })) {
+                            my $prot = $c->req->uri->secure ? 'https://' : 'http://';
+                            my $port = $c->req->uri->port;
+                            $c->res->redirect("${prot}${fullhost}:${port}${url}");
+                            $c->detach;
+                        }
+                    }
+                }
+
                 # if the mobile page does not exist
                 # else, render as normal
                 if (not $page) {
