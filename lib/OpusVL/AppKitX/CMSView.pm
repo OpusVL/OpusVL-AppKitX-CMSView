7809e84e403f22eaae6fc61363c56f70131e6644
Colin Newell <colin.newell@gmail.com>
2014-03-14 11:04:48 +0000

    Documented the error page

--- lib/OpusVL/AppKitX/CMSView.pm
+++ lib/OpusVL/AppKitX/CMSView.pm
@@ -80,13 +80,30 @@ OpusVL::AppKitX::CMSView - CMS front end
 
 =head1 METHODS
 
+=head2 finalize_error
+
+The CMS view overrides this method to provide a custom error page.
+
+First it looks for a page on the site with the url /500.  If that can not be found
+it checks to see if there is a template file defined in the custom_error_template
+config setting.  If not it reverts to using it's standard 500.tt provided with this
+module.
+
+If you provide your own template using the custom_error_template you need
+to be aware that the TT is setup to look in the root directory rather than 
+root/templates.  This is because the usual directory search path hookup isn't performed
+since it's a different view to the usual AppKitTT view.
+
+Catalyst logs the real error as an [error] message so this hook doesn't attempt
+to log the error again.
+
 =head1 BUGS
 
 =head1 AUTHOR
 
 =head1 COPYRIGHT and LICENSE
 
-Copyright (C) 2012 OpusVL
+Copyright (C) 2012 - 2014 OpusVL
 
 This software is licensed according to the "IP Assignment Schedule" provided with the development project.
 
