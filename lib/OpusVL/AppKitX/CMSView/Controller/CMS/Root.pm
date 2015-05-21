4d532817edb8b88fb443ab67864b2513f74cfeaa
Brad Haywood <brad.haywood@opusvl.com>
2015-05-21 09:33:14 +0100

    Added markdown support

--- lib/OpusVL/AppKitX/CMSView/Controller/CMS/Root.pm
+++ lib/OpusVL/AppKitX/CMSView/Controller/CMS/Root.pm
@@ -177,10 +177,10 @@ sub render_page
 
     if (my $template = $page->template->content) {
         if ($display_errors) {
-            $template = '[% BLOCK content %]' . $display_errors . $page->content . '[% END %]' . $template;
+            $template = '[% BLOCK content %][% USE Markdown -%][% FILTER markdown %]' . $display_errors . $page->content . '[%- END %][% END %]' . $template;
         }
         else {
-            $template = '[% BLOCK content %]' . $page->content . '[% END %]' . $template;
+            $template = '[% BLOCK content %][% USE Markdown -%][% FILTER markdown %]' . $page->content . '[%- END %][% END %]' . $template;
         }
         $c->stash->{template}   = \$template;
         $c->stash->{no_wrapper} = 1;
